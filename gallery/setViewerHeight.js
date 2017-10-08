f0854a6e2d53d0cb08a1cc69be7ce76115b7c95d
Joe Hos <joehoskisson@gmail.com>
2017-10-07 20:51:05 -0600

    made resize event only fire at 15fps, to make it less expensive. Followed docs on mozilla developers network for resize event.

--- gallery/setViewerHeight.js
+++ gallery/setViewerHeight.js
@@ -6,28 +6,40 @@ $(document).ready(function(){
     document.getElementById("viewer").style.height = window.innerHeight + "px";
   }
 
-  // Do this again when window resized
-  // especially thinking of turning phone to landscape mode
-  window.addEventListener("resize", function(){
-    console.log("resize event fired");
-    if(window.innerWidth <= 768) {
-      var viewer = document.getElementById("viewer");
-      viewer.style.height = window.innerHeight + "px";
+  (function() {
+    window.addEventListener("resize", resizeThrottler, false);
 
-      // if in landscape mode (or screen wider than it is tall) scroll to top of
-      // viewer
-      if(window.innerWidth > window.innerHeight){
-        var rect = viewer.getBoundingClientRect();
-        // since rect.top is relative to viewport, add vertical scroll position.
-        var viewerTop = rect.top + window.scrollY;
-        console.log(viewerTop);
-        window.scroll(0, viewerTop);
+    var resizeTimeout;
+    function resizeThrottler() {
+      // ignore resize events as long as an actualResizeHandler execution is in
+      // the queue
+      if(!resizeTimeout) {
+        resizeTimout = setTimeout(function() {
+          resizeTimeout = null;
+          actualResizeHandler();
+
+          // The actualResizeHandler will execute at a rate of 15fps
+        }, 66);
       }
-
-
     }
 
+    function actualResizeHandler() {
+      console.log("resize event fired");
+      if(window.innerWidth <= 768) {
+        var viewer = document.getElementById("viewer");
+        viewer.style.height = window.innerHeight + "px";
 
-  });
+        // if in landscape mode (or screen wider than it is tall) scroll to top of
+        // viewer
+        if(window.innerWidth > window.innerHeight){
+          var rect = viewer.getBoundingClientRect();
+          // since rect.top is relative to viewport, add vertical scroll position.
+          var viewerTop = rect.top + window.scrollY;
+          console.log(viewerTop);
+          window.scroll(0, viewerTop);
+        }
+      }
+    }
+  }());
 
 })
