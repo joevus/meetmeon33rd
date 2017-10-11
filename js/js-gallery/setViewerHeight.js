02314778cf507c8f9df3209eb63f646eb49c639d
Joe Hos <joehoskisson@gmail.com>
2017-10-11 16:26:40 -0600

    Added padding to viewer container to give wiggle room for fullscreen feel on landscape mobile. Fixed bug in setViewerHeight.

--- js/js-gallery/setViewerHeight.js
+++ js/js-gallery/setViewerHeight.js
@@ -1,11 +1,5 @@
 /* Desired changes
-    - Make screen scroll top top of viewer when transition to landscape mode
-      - the problem isn't window.scroll, it works sometimes. Perhaps a timing
-      problem, async.
-      - actually, probably don't want to scroll to top of viewer when resize
-      event fires, it moves the screen to the viewer anytime the toolbars appear
-      or disappear. Like you cannot escape the viewer in landscape mode unless
-      fix it with more code. Maybe for now, forgo this feature.
+
     - Make the viewer less than 100% of screen height when in mobile potrait mode
     - Make the page not scroll up when user means to swipe left or right on viewer
     - Give a border, same color as background, to viewer so that when in
@@ -46,7 +40,7 @@ $(document).ready(function(){
         //   var viewerTop = rect.top + window.scrollY;
         //   window.scrollTo(0, viewerTop);
         // }
-      if(window.innerHeight > 768) {
+      if(window.innerWidth > 768) {
         // set screen back to size for anything larger than xs (768px)
         // mainly for if desktop user makes window small,
         // then large again.
@@ -65,7 +59,8 @@ $(document).ready(function(){
       var viewer = document.getElementById("viewer");
       if(window.innerWidth < window.innerHeight) {
         // less than full viewport height if portrait mode
-        viewer.style.height = window.innerHeight * 0.85 + "px";
+        // viewer.style.height = window.innerHeight * 0.85 + "px";
+        viewer.style.height = "350px";
       } else {
         // full viewport height when landscape mode
         viewer.style.height = window.innerHeight + "px";
