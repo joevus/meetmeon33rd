daa3173e92a2a230b0c0fc719d351329cab0df47
Joe Hos <joehoskisson@gmail.com>
2017-10-10 19:50:17 -0600

    discontinued scrolling to top of viewer when switch to lanscape mode for now. Also rearranged some repeated code into function.

--- js/js-gallery/setViewerHeight.js
+++ js/js-gallery/setViewerHeight.js
@@ -1,10 +1,17 @@
+/* Desired changes
+    - Make screen scroll top top of viewer when transition to landscape mode
+      - the problem isn't window.scroll, it works sometimes. Perhaps a timing
+      problem, async.
+      - actually, probably don't want to scroll to top of viewer when resize
+      event fires, it moves the screen to the viewer anytime the toolbars appear
+      or disappear. Like you cannot escape the viewer in landscape mode unless
+      fix it with more code. Maybe for now, forgo this feature.
+    - Make the viewer less than 100% of screen height when in mobile potrait mode
+    - Make the page not scroll up when user means to swipe left or right on viewer
+*/
 $(document).ready(function(){
-  console.log("viewer height read");
-  console.log(window.innerHeight);
-  // for size mobile, make viewer's height the full height of screen
-  if(window.innerWidth <= 768) {
-    document.getElementById("viewer").style.height = window.innerHeight + "px";
-  }
+
+  checkAndSetMobileScreenSize();
 
   (function() {
     window.addEventListener("resize", resizeThrottler, false);
@@ -24,24 +31,43 @@ $(document).ready(function(){
     }
 
     function actualResizeHandler() {
-      console.log("resize event fired");
-      if(window.innerWidth <= 768) {
-        var viewer = document.getElementById("viewer");
-        viewer.style.height = window.innerHeight + "px";
 
+      checkAndSetMobileScreenSize()
+
+        // **Discontinue feature for now**
         // if in landscape mode (or screen wider than it is tall) scroll to top of
         // viewer
-        if(window.innerWidth > window.innerHeight){
-          var rect = viewer.getBoundingClientRect();
-          // since rect.top is relative to viewport, add vertical scroll position.
-          var viewerTop = rect.top + window.scrollY;
-          console.log(viewerTop);
-          window.scroll(0, viewerTop);
-        }
-      } else {
+        // if(window.innerWidth > window.innerHeight){
+        //   var rect = viewer.getBoundingClientRect();
+        //   // since rect.top is relative to viewport, add vertical scroll position.
+        //   var viewerTop = rect.top + window.scrollY;
+        //   window.scrollTo(0, viewerTop);
+        // }
+      if(window.innerHeight > 768) {
+        // set screen back to size for anything larger than xs (768px)
+        // mainly for if desktop user makes window small,
+        // then large again.
         document.getElementById("viewer").style.height = "640px";
       }
+
     }
   }());
+
+
+  // Check if mobile size and whether portrait or landscape. Set screen size
+  // accordingly.
+
+  function checkAndSetMobileScreenSize() {
+    if(window.innerWidth <= 768) {
+      var viewer = document.getElementById("viewer");
+      if(window.innerWidth < window.innerHeight) {
+        // less than full viewport height if portrait mode
+        viewer.style.height = window.innerHeight * 0.85 + "px";
+      } else {
+        // full viewport height when landscape mode
+        viewer.style.height = window.innerHeight + "px";
+      }
+    }
+  }
 
 })
